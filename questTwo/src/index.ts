2e80e5948583646dd972cd84221a515e57ec4899
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-04-23 01:28:48 +0200

    typed graphQL construction

--- questTwo/src/index.ts
+++ questTwo/src/index.ts
@@ -1,45 +1,65 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import "reflect-metadata"; 
+import {Arg, Field, InputType, Mutation, ObjectType, Query, Resolver} from 'type-graphql'
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+  @ObjectType()
+class Book {
+    @Field()
+    id: string
+    @Field()
+    title: string
+    @Field()
+    author: string
+}
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
+const books :Book[] = [
   {
+    id: "0",
     title: 'The Awakening',
     author: 'Kate Chopin',
   },
   {
+    id: "1",
     title: 'City of Glass',
     author: 'Paul Auster',
   },
 ];
 
+
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+@Resolver(Book)
+class BookResolver  {
+  @Query(() => Book)
+  getBookById(@Arg("id") id: string) {
+    return books.find((book) => book.id == id);
+  }
+  
+  @Mutation(() => Book)
+  addBook(@Arg("data") {title, author}: BookInput) {
+    const lastId = parseInt(books.at(-1).id, 10);
+    const id = (lastId + 1).toString();
+    books.push({
+      title,
+      author,
+      id,
+    });
+    return books.at(-1);
+  };
+}
+  
+  @InputType()
+  class BookInput {
+    @Field()
+    title: string;
 
+    @Field()
+    author:string;
+  }
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
