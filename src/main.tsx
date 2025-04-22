f12ceca058cba122e9848976ec26a631780dd3f4
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-04-22 11:36:48 +0200

    install in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+} from "@apollo/client"; 
+ const client = new ApolloClient({
+    uri: 'https://spacex-production.up.railway.app/', 
+    cache: new InMemoryCache()
+  }); 
+
 createRoot(document.getElementById('root')!).render(
+ 
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
