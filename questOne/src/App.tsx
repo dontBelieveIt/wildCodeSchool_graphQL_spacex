c6d1d4594238be3772579b5fab0c869b3cc47318
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-04-22 11:57:05 +0200

    end of quest one, beginning of quest two

---
+++ questOne/src/App.tsx
@@ -0,0 +1,54 @@
+import './App.css'
+import {
+  useQuery, 
+  gql, 
+} from "@apollo/client"; 
+
+ 
+function App() {
+ type LaunchType = {
+    id: number;
+    launch_date_utc : string ;
+    launch_success : boolean;
+    rocket_name : string; 
+    video_link : string; 
+    details : Text; 
+  }
+
+  const GET_LAUNCHES = gql`
+  query GetLauches {
+    launches(limit: 5) {
+      id 
+      launch-date-utc
+      launch_success
+      rocket {
+        rocket_name
+        }
+        links {
+        video_link
+        }
+        details
+        }
+        }`;
+
+  const { loading, error, data } = useQuery(GET_LAUNCHES); 
+
+  if (loading) {
+    return <p>Loading...</p>; 
+  }
+  if (error) {
+    return <p>Error :</p>
+  }
+
+  return (
+    <>
+      <div className='App'>
+        {data.launches.map((launch:LaunchType) => (
+          <li key={launch.id}>{launch.launch_date_utc}</li>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
