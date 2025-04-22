6d7b596a3fa15c88ce0ff39f5f26ade6f480d9ce
Roxane <160009049+dontBelieveIt@users.noreply.github.com>
2025-04-22 12:05:02 +0200

    end of quest one : result appearing in vite App

--- questOne/src/App.tsx
+++ questOne/src/App.tsx
@@ -16,39 +16,32 @@ function App() {
   }
 
   const GET_LAUNCHES = gql`
-  query GetLauches {
+  query GetLaunches {
     launches(limit: 5) {
-      id 
-      launch-date-utc
+      id
+      launch_date_utc
       launch_success
       rocket {
         rocket_name
-        }
-        links {
+      }
+      links {
         video_link
-        }
-        details
-        }
-        }`;
+      }
+      details
+    }
+  }
+`;
 
   const { loading, error, data } = useQuery(GET_LAUNCHES); 
-
-  if (loading) {
-    return <p>Loading...</p>; 
-  }
-  if (error) {
-    return <p>Error :</p>
-  }
-
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
   return (
-    <>
-      <div className='App'>
-        {data.launches.map((launch:LaunchType) => (
-          <li key={launch.id}>{launch.launch_date_utc}</li>
-        ))}
-      </div>
-    </>
-  )
+    <div className="App">
+      {data.launches.map((launch:LaunchType) => (
+        <li key={launch.id}>{launch.launch_date_utc}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App
--- questOne/src/main.tsx
+++ questOne/src/main.tsx
@@ -14,7 +14,6 @@ import {
   }); 
 
 createRoot(document.getElementById('root')!).render(
- 
   <StrictMode>
     <ApolloProvider client={client}>
       <App />
